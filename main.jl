76c3dbcf30067ff0c15a26a25134d356472eade3
Rakhyvel <josephmshimel@gmail.com>
2025-11-17 01:18:38 -0700

    pivot to evolutionary GA

--- main.jl
+++ main.jl
@@ -1,6 +1,7 @@
-using CSV, DataFrames, JuMP, Ipopt
+using CSV, DataFrames, Evolutionary, DataStructures
 
-b = [1800.0, 160.0, 100.0, 100.0]
+lower = [1700.0, 160.0, 60.0, 80.0, 25.0, 400, 11, 15]
+upper = [1800.0, Inf,   160.0, Inf, Inf, 420, 40, 100]
 
 struct Ingredient
     name::String
@@ -8,15 +9,19 @@ struct Ingredient
     protein::Float64
     carbs::Float64
     fat::Float64
+    fiber::Float64
+    magnesium::Float64
+    zinc::Float64
+    vitd::Float64
 end
 
 # Ingredient x nutrient matrix
 df = CSV.read("ingredients.csv", DataFrame; comment="#")
 ingredients = [
-    Ingredient(row.name, row.calories, row.protein, row.carbs, row.fats)
+    Ingredient(row.name, row.calories, row.protein, row.carbs, row.fats, row.fiber, row.Mg, row.Zn, row["Vit D"])
     for row in eachrow(df)
 ]
-num_nutrients = 4
+num_nutrients = 8
 num_ingredients = length(ingredients)
 
 # JuMP needs a matrix, so set one up
@@ -26,28 +31,49 @@ for (j, ingr) in enumerate(ingredients)
     A[2, j] = ingr.protein
     A[3, j] = ingr.carbs
     A[4, j] = ingr.fat
+    A[5, j] = ingr.fiber
+    A[6, j] = ingr.magnesium
+    A[7, j] = ingr.zinc
+    A[8, j] = ingr.vitd
 end
 
-# setup the model
-model = Model(Ipopt.Optimizer)
-set_silent(model)
-@variable(model, 0 <= x[1:num_ingredients] <= 2)
-ε = 1
-@objective(model, Min,
-    sum(((A*x - b) ./ b).^2)
-    + 0.01 * sum(exp(-10 * x[j]) for j=1:num_ingredients)
-)
-# optimize
-optimize!(model)
-x_opt = value.(x)
-
-# print out results
-real_x_opt = zeros(Float64, num_nutrients)
-for i in 1:num_ingredients
-    ingredient_amount = x_opt[i]
-        name = ingredients[i].name
-        rounded_amount = round(ingredient_amount * 100, digits=1)
-        println("$name: $(rounded_amount)g")
+epsilon = 0.2
+function fitness(x)
+    real_x = ifelse.(x .>= epsilon, x, 0.0)
+    weight = sum(real_x)
+    nutrients = A * real_x
+    violation = max.(0.0, lower .- nutrients) + max.(0.0, nutrients .- upper)
+    return sum(violation .^ 2) + max(0.0, weight - 6.0)
 end
 
-println(A*x_opt)
+counter = DefaultDict{String, Int}(0)
+for iter in 1:500
+    res = Evolutionary.optimize(
+        fitness,
+        zeros(Float64, num_ingredients),
+        CMAES(),
+        Evolutionary.Options(
+            iterations=2000,
+        )
+    )
+
+    x_opt = res.minimizer
+    real_x = ifelse.(x_opt .>= epsilon, x_opt, 0.0)
+
+    # print out results
+    for i in 1:num_ingredients
+        ingredient_amount = real_x[i]
+        if ingredient_amount > 0
+            name = ingredients[i].name
+            rounded_amount = round(ingredient_amount * 100, digits=1)
+            counter[name] += 1
+            # println("$name ($freq): $(rounded_amount)g")
+        end
+    end
+
+    for (key, val) in sort(collect(counter), by = x -> -x[2], rev = true)
+        println("$key => $val")
+    end
+
+    println(A*real_x)
+end
