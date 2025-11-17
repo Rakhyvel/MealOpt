3b5f7ff7080c5f66116ccc1f259ec4689e5f21e2
Rakhyvel <josephmshimel@gmail.com>
2025-11-16 23:31:47 -0700

    add bounds constraint

--- main.jl
+++ main.jl
@@ -1,6 +1,6 @@
 using CSV, DataFrames, JuMP, Ipopt
 
-target_nutrients = [1800.0, 160.0, 100.0, 100.0]
+b = [1800.0, 160.0, 100.0, 100.0]
 
 struct Ingredient
     name::String
@@ -16,10 +16,11 @@ ingredients = [
     Ingredient(row.name, row.calories, row.protein, row.carbs, row.fats)
     for row in eachrow(df)
 ]
+num_nutrients = 4
 num_ingredients = length(ingredients)
 
 # JuMP needs a matrix, so set one up
-A = zeros(Float64, 4, num_ingredients)
+A = zeros(Float64, num_nutrients, num_ingredients)
 for (j, ingr) in enumerate(ingredients)
     A[1, j] = ingr.calories
     A[2, j] = ingr.protein
@@ -30,20 +31,23 @@ end
 # setup the model
 model = Model(Ipopt.Optimizer)
 set_silent(model)
-@variable(model, 0 <= decision[1:num_ingredients] <= 1000)
-@objective(model, Min, sum(((A * decision - target_nutrients) ./ target_nutrients).^2))
-
+@variable(model, 0 <= x[1:num_ingredients] <= 2)
+ε = 1
+@objective(model, Min,
+    sum(((A*x - b) ./ b).^2)
+    + 0.01 * sum(exp(-10 * x[j]) for j=1:num_ingredients)
+)
 # optimize
 optimize!(model)
-x_opt = value.(decision)
+x_opt = value.(x)
 
 # print out results
+real_x_opt = zeros(Float64, num_nutrients)
 for i in 1:num_ingredients
-    ingredient_amount = x_opt[i] * 100
-    EPSILON = 0.1
-    if ingredient_amount > EPSILON
+    ingredient_amount = x_opt[i]
         name = ingredients[i].name
-        rounded_amount = round(ingredient_amount, digits=1)
+        rounded_amount = round(ingredient_amount * 100, digits=1)
         println("$name: $(rounded_amount)g")
-    end
 end
+
+println(A*x_opt)
